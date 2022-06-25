8354879cfa878b8143d49e4752593c97aa4df132
Abdulai <babdulai149@gmail.com>
2022-06-25 23:29:14 +0000

    .

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+ let str = string.split('').reverse().join('');
+ return str;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arg, ...arr) {
+ for(let i = 0; i < arr.length; i++){
+    if(arr[i] === arg){
+        arr[i].splice(i, 1);
+    }
+ }
 };
 
 // Do not edit below this line
